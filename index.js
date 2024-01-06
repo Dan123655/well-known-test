00426a4def6f246fe485e7124c47b58533a4f4d0
Dan123655 <106719160+Dan123655@users.noreply.github.com>
2024-01-06 23:26:58 +0100

    adresar well known test

--- index.js
+++ index.js
@@ -17,15 +17,30 @@ app.get("/.well-known/assetlinks.json", (req, res) => {
       relation: ["delegate_permission/common.handle_all_urls"],
       target: {
         namespace: "android_app",
-        package_name: "com.map.motorcyclistmap.com",
+        package_name: "com.mapotic.adresarfarmaru",
         sha256_cert_fingerprints: [
-          "7B:6A:92:33:27:FE:F6:E8:14:E0:D9:66:73:9F:C9:04:6B:75:30:77:94:13:84:13:5F:96:77:7B:BF:30:B4:94",
-          "48:4F:E8:58:F4:3F:4F:3B:A2:FA:0D:82:55:8B:F9:7A:86:1B:A6:8A:C5:AD:00:6E:DF:29:B1:FC:20:3E:CB:5F",
+          "0C:42:66:F3:D8:0B:BE:F3:E2:0C:8C:4F:B5:2C:0E:34:C9:6C:04:8A:A2:34:99:5E:45:E9:AD:44:BA:06:E1:6C",
         ],
       },
     },
   ]);
 });
+// app.get("/.well-known/assetlinks.json", (req, res) => {
+//   console.log("assetlinks.json");
+//   res.json([
+//     {
+//       relation: ["delegate_permission/common.handle_all_urls"],
+//       target: {
+//         namespace: "android_app",
+//         package_name: "com.map.motorcyclistmap.com",
+//         sha256_cert_fingerprints: [
+//           "7B:6A:92:33:27:FE:F6:E8:14:E0:D9:66:73:9F:C9:04:6B:75:30:77:94:13:84:13:5F:96:77:7B:BF:30:B4:94",
+//           "48:4F:E8:58:F4:3F:4F:3B:A2:FA:0D:82:55:8B:F9:7A:86:1B:A6:8A:C5:AD:00:6E:DF:29:B1:FC:20:3E:CB:5F",
+//         ],
+//       },
+//     },
+//   ]);
+// });
 
 app.get("/.well-known/apple-app-site-association", (req, res) => {
   // Set content type to application/json
